74a4d758ed28880c6f3090c6bc22faacbaac30a2
fooles <18730471450@163.com>
2016-11-17 12:51:44 +0800

    remove binary.

--- main.go
+++ main.go
@@ -65,11 +65,13 @@ func checkSignature(r *http.Request) bool {
 	//这个用来得到最终的散列值的字符切片。Sum 的参数可以用来都现有的字符切片追加额外的字节切片：一般不需要要。
 	bs := h.Sum(nil)
 	//SHA1 值经常以 16 进制输出，例如在 git commit 中。使用%x 来将散列结果格式化为 16 进制字符串。
+
 	fmt.Println(hex.EncodeToString(bs))
-	fmt.Printf("%x\n", bs)
+	fmt.Printf("%s\n", signature)
+
+	fmt.Println(hex.EncodeToString(bs) == signature)
 
 	if hex.EncodeToString(bs) == signature {
-
 		return true
 	} else {
 		return false
