d80970917443998019ff9874b07b85304e8ab390
fooles <18730471450@163.com>
2016-11-17 11:55:03 +0800

    acc

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/sha1"
+	"encoding/hex"
 	"fmt"
 	"log"
 	"net/http"
@@ -64,9 +65,10 @@ func checkSignature(r *http.Request) bool {
 	//这个用来得到最终的散列值的字符切片。Sum 的参数可以用来都现有的字符切片追加额外的字节切片：一般不需要要。
 	bs := h.Sum(nil)
 	//SHA1 值经常以 16 进制输出，例如在 git commit 中。使用%x 来将散列结果格式化为 16 进制字符串。
-	fmt.Println(string(bs))
+	fmt.Println(hex.EncodeToString(bs))
 	fmt.Printf("%x\n", bs)
-	if string(bs) == signature {
+
+	if hex.EncodeToString(bs) == signature {
 
 		return true
 	} else {
