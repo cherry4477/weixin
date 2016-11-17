fe66f03a63038407fcd04bc0e1fe14df0aa5869c
fooles <18730471450@163.com>
2016-11-17 15:29:04 +0800

    remove binary.

--- main.go
+++ main.go
@@ -3,14 +3,69 @@ package main
 import (
 	"crypto/sha1"
 	"encoding/hex"
+	"encoding/json"
 	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"sort"
 	"strings"
+	"sync"
+	"time"
 )
+
+var (
+	token      string
+	tokenMutex sync.Mutex
+)
+
+func gettoken() string {
+	tokenMutex.Lock()
+	defer tokenMutex.Unlock()
+	return token
+
+}
+
+func updatatoken() {
+
+	f := func() {
+		v := url.Values{}
+		v.Set("grant_type", "client_credential")
+		v.Set("appid", "wxd653a9d6ef5659ab")
+		v.Set("secret", "114967dd70de0c89469d94f3ef493d35")
+		//url:=url.URL
+		r, err := http.Get("https://api.weixin.qq.com/cgi-bin/token?" + v.Encode())
+		if err != nil {
+			return
+		}
+		if r != nil {
+			defer r.Body.Close()
+		}
+
+		data, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			return
+		}
+		//GetResponseData(r)
+		//json.Unmarshal
+		var params = struct {
+			Access  string `json:"access_token"`
+			Expires int64  `json:"expires_in"`
+		}{}
+		err = json.Unmarshal(data, &params)
+		if err != nil {
+			return
+		}
+		log.Println("params", params)
+
+	}
+	f()
+	for range time.Tick(time.Hour) {
+		f()
+	}
+}
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()       //解析参数，默认是不会解析的
@@ -83,6 +138,7 @@ func follow(w http.ResponseWriter, r *http.Request) {
 
 }
 func main() {
+	go updatatoken()
 	http.HandleFunc("/", sayhelloName) //设置访问的路由
 	http.HandleFunc("/interface", weixinin)
 	http.HandleFunc("/follow", follow)
@@ -92,6 +148,14 @@ func main() {
 	}
 }
 
+func GetResponseData(r *http.Response) ([]byte, error) {
+	if r != nil {
+		defer r.Body.Close()
+	}
+
+	return ioutil.ReadAll(r.Body)
+
+}
 func GetRequestData(r *http.Request) ([]byte, error) {
 	if r.Body == nil {
 		return nil, nil
@@ -109,13 +173,10 @@ func checkSignature(r *http.Request) bool {
 	signature := r.FormValue("signature")
 	timestamp := r.FormValue("timestamp")
 	nonce := r.FormValue("nonce")
-
 	token := "winxin"
 	tmpArr := sort.StringSlice{token, timestamp, nonce}
 	sort.Sort(tmpArr)
 	tmpStr := strings.Join(tmpArr, "")
-
-	//s := "sha1 this string"
 	//产生一个散列值得方式是 sha1.New()，sha1.Write(bytes)，然后 sha1.Sum([]byte{})。这里我们从一个新的散列开始。
 	h := sha1.New()
 	//写入要处理的字节。如果是一个字符串，需要使用[]byte(s) 来强制转换成字节数组。
