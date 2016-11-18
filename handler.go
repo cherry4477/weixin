56118ebf3960ce6ac42a03199ed8c5a41eab094b
fooles <18730471450@163.com>
2016-11-18 17:00:41 +0800

    Docker

--- handler.go
+++ handler.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"encoding/xml"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -18,26 +19,75 @@ func follow(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		return
 	}
-	r.ParseForm()       //解析参数，默认是不会解析的
-	log.Println(r.Form) //这些信息是输出到服务器端的打印信息
+	r.ParseForm() //解析参数，默认是不会解析的
+	//log.Println(r.Form) //这些信息是输出到服务器端的打印信息
 	log.Println("From", r.RemoteAddr, r.Method, r.URL.RequestURI(), r.Proto)
 
 	data, err := GetRequestData(r)
 	if err != nil {
 		return
 	}
+	//createtime
 	var common = struct {
 		FromUserName string `xml:"FromUserName"`
 		MsgType      string `xml:"MsgType"`
 		Event        string `xml:"Event"`
+		CreateTime   int64  `xml:"CreateTime"`
 	}{}
 
 	err = xml.Unmarshal(data, &common)
 	if err != nil {
 		return
 	}
+
 	if common.MsgType == "event" {
 		if common.Event == "subscribe" {
+
+			var send = struct {
+				OpenID      string `json:"openId"`
+				ProvideTime int64  `json:"provideTime"`
+			}{
+				OpenID:      common.FromUserName,
+				ProvideTime: common.CreateTime,
+			}
+
+			data, err = json.Marshal(&send)
+
+			if err != nil {
+				return
+			}
+
+			resp, data, err := RemoteCallWithBody(
+				"POST",
+				"https://datafoundry.coupon.app.dataos.io/charge/v1/provide/coupons?number=1&amount=69",
+				"",
+				"",
+				data,
+				"application/json; charset=utf-8",
+			)
+			if err != nil {
+				return
+			}
+
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				return
+			}
+
+			var card = struct {
+				IsProvide bool   `json:"isProvide"`
+				Code      string `json:"code"`
+			}{}
+
+			err = json.Unmarshal(body, &card)
+			if err != nil {
+				return
+			}
+
+			if card.IsProvide {
+				return
+			}
+
 			type one struct {
 				Content string `json:"content"`
 			}
@@ -49,7 +99,7 @@ func follow(w http.ResponseWriter, r *http.Request) {
 				Touser:  common.FromUserName,
 				Msgtype: "text",
 				Text: one{
-					Content: "Hello text",
+					Content: card.Code,
 				},
 			}
 
